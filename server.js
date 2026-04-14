1d5c7d2c27dc4c024e18bb475c0b4f6ab2a677df
javedagwan02 <javedagwan02@gmail.com>
2026-04-15 00:32:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get("/start-game", async (req,res)=>{
 const response = await axios.post(
   "https://game.gamblly-api.com/production/v1/gameLaunch.php",
   {
-    member_account: userId,
+    member_account: userId.replace(/[^a-zA-Z0-9]/g, "")
     game_uid: "a990de177577a2e6a889aaac5f57b429",
     api_key: "fecfaa08d7aCodeHub944b04ac2cf59a",
     currency_code: "INR",
